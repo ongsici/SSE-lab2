033d02a07f8b225dfa2b52c6a58a13fb7c4c0df1
ongsici <sco115@ic.ac.uk>
2024-10-14 15:24:23 +0100

    feat: gender and happiness toggle

--- src/app.py
+++ src/app.py
@@ -9,6 +9,11 @@ def hello_world():
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
-    return render_template("hello.html", name=input_name, age=input_age)
-
-
+    input_gender = request.form.get("gender")
+    input_happiness = request.form.get("happy")
+    if int(input_happiness) >= 0:
+        return render_template("hello.html", name=input_name, age=input_age, gender=input_gender, happy=input_happiness)
+    else:
+        return render_template("sad.html", name=input_name, age=input_age, gender=input_gender, happy=input_happiness)
+if __name__ == '__main__':  
+   app.run(host= "0.0.0.0",port=8000)  
