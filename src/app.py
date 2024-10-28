7ad91a67960458818353c2d955f46a362b29f9f0
ongsici <sco115@ic.ac.uk>
2024-10-28 15:29:47 +0000

    prime

--- src/app.py
+++ src/app.py
@@ -11,7 +11,33 @@ def is_cube(n):
     return int(round(n ** (1/3))) ** 3 == n
 
 
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
 def process_query(input):
+    if "numbers are primes" in input:
+        input = input.replace("?", "")
+        value = input.split(":")[-1].split(",")
+        result = []
+        for number in value:
+            number = number.strip()
+            if is_prime(number):
+                result.append(number)
+        return ",".join(result)
+
     if "minus" in input:
         new_input = input.replace("?", "")
         value1 = int(new_input.split(" ")[2])
