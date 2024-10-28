57c3db1c79c5535e90ef765dc0d6b8acefba06d9
ongsici <sco115@ic.ac.uk>
2024-10-28 14:05:42 +0000

    chore: fixed formatting

--- src/test_app.py
+++ src/test_app.py
@@ -10,6 +10,7 @@ def test_knows_about_dinosaurs():
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
 
+
 def test_what_is_your_name():
     assert process_query("What is your name?") == "SiCi"
 
